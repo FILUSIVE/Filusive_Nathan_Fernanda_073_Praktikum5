5969b1aadc33d47ac54d61c36527b261bdf8bf51
FILUSIVE <90390288+FILUSIVE@users.noreply.github.com>
2021-11-14 17:46:21 +0700

    Add files via upload

---
+++ Program 24.cpp
@@ -0,0 +1,22 @@
+/* Program Dua Puluh Empat */
+/* Program Sederhana Menggunakan Fungsi yang tidak mengembalikan nilai */
+
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+void garis ();
+main ()
+{
+	system ("cls");
+	garis ();
+	printf ("NO NIM NAMA NILAI\n ");
+	garis ();
+	getch();
+}
+void garis ()
+{
+	printf("=============================\n");
+}
